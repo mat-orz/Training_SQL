71f9fe18d4bfb21b8f3331af401953c4d0012cbf
mat-orz <mat-orz>
2018-10-20 22:31:24 +0200

    wip post

--- app.py
+++ app.py
@@ -26,36 +26,11 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app)
 
-BOOKS = [
-    {
-        'title': 'On the Road',
-        'author': 'Jack Kerouac',
-        'read': True
-    },
-    {
-        'title': 'Harry Potter and the Philosopher\'s Stone',
-        'author': 'J. K. Rowling',
-        'read': False
-    },
-    {
-        'title': 'Green Eggs and Ham',
-        'author': 'Dr. Seuss',
-        'read': True
-    }
-]
-
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
 
-
-@app.route('/books', methods=['GET'])
-def all_books():
-    return jsonify({
-        'status': 'success',
-        'books': BOOKS
-    })
 
 
 @app.route('/date', methods=['GET'])
